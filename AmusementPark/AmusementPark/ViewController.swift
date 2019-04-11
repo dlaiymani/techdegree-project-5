424c907a3657da1b0bc17bbcc1a03320d2f82515
David laiymani <david.laiymani@gmail.com>
2019-04-11 17:21:05 +0200

    Initial commit

---
+++ AmusementPark/AmusementPark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AmusementPark
+//
+//  Created by davidlaiymani on 11/04/2019.
+//  Copyright © 2019 davidlaiymani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
